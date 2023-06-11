85a4b443033a0dca2a16d1486533b4b527bc9f7f
AbinandhMJ <abinandhmurukesan@gmail.com>
2023-06-11 14:56:27 +0530

    One-To-One Link, UserProfile and Migrations

--- crm_main/settings.py
+++ crm_main/settings.py
@@ -45,7 +45,8 @@ THIRD_PARTY_APPS =[
  ]
 
 LOCAL_APPS =[
-     # add local apps which you create using startapp
+     'apps.common',
+     'apps.userprofile',
  ]
 
  # Application definition
@@ -81,6 +82,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'crm_main.wsgi.application'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -117,8 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # Static files (CSS, JavaScript, Images)
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media Files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -128,6 +139,7 @@ try:
     from crm_main.local_settings import *
 except ImportError:
     pass
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
